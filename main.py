c14b9903c1ce363c4299155248a421c2e76464f6
seryoungshim17 <seryoungshim07@gmail.com>
2021-09-13 13:22:34 +0000

    add hyperparameter arguments

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from torchvision.datasets import FashionMNIST
 from torchvision import transforms
 import torch
 
-def main():
+import argparse
+
+def main(**kwargs):
     transform = transforms.Compose([
         transforms.ToTensor()
     ])
@@ -22,4 +24,41 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="HyperParameter")
+#     parser.add_argument(
+#         "--PATH",
+#         type = str,
+#         help = "Base Path",
+#         default = "input/"
+#     )
+    parser.add_argument(
+        "--OPTIMIZER",
+        type = str,
+        help = "Optimizer(Adam/SGD)",
+        default = "Adam"
+    )
+    parser.add_argument(
+        "--LR",
+        type = float,
+        help = "Learning rate",
+        default = 1e-5
+    )
+    parser.add_argument(
+        "--EPOCH",
+        type = int,
+        help = "Total Training Epoch",
+        default = 20
+    )
+    parser.add_argument(
+        "--BATCH_SIZE",
+        type = int,
+        help = "Batch Size",
+        default = 128
+    )
+    arg = parser.parse_args()
+    main(
+        optim=arg.OPTIMIZER,
+        learning_rate=arg.LR,
+        epoch=arg.EPOCH,
+        batch_size=arg.BATCH_SIZE
+    )
